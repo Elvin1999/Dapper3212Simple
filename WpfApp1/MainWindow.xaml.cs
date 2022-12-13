b69ae7fba201645a65089f43487483c6a6458b73
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-12-13 20:00:30 +0400

    done without insert

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,12 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -29,17 +32,52 @@ namespace WpfApp1
         {
             List<Player> players = new List<Player>();
             var conn = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
-            using (var connection=new SqlConnection(conn))
+            using (var connection = new SqlConnection(conn))
             {
-                players=connection.Query<Player>("SELECT Id,Name,Score,IsStar FROM Players").ToList();
+                players = connection.Query<Player>("SELECT Id,Name,Score,IsStar FROM Players").ToList();
             }
             return players;
         }
+
+        public Player GetById(int id)
+        {
+            var conn = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            using (var connection = new SqlConnection(conn))
+            {
+                var player = connection.QueryFirstOrDefault<Player>("SELECT * FROM Players WHERE Id=@PId",
+                    new { PId = id });
+                return player;
+            }
+        }
+
+
+        public void Update(Player player)
+        {
+            var conn = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            using (var connection=new SqlConnection(conn))
+            {
+                connection.Execute(@"
+UPDATE Players
+SET Name=@PName,Score=@PScore,IsStar=@PIsStar
+WHERE Id=@Pid",
+new { PName = player.Name, PScore = player.Score, PIsStar = player.IsStar, PId = player.Id });
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            var players=GetAll();
+            var player = GetById(1);
+            player.Name = "Rafiq";
+            player.Score = 10;
+
+            Update(player);
+
+            var players = GetAll();
             myDataGrid.ItemsSource = players;
+            ////var player = GetById(1);
+            ////myDataGrid.ItemsSource = new List<Player> {player };
+
         }
     }
 }
