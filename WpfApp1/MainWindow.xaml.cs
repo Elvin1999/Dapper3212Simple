b24d3cb8ca85033da9585f03052685cb081dd544
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2022-12-13 19:41:23 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Entities;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Player> GetAll()
+        {
+            List<Player> players = new List<Player>();
+            var conn = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            using (var connection=new SqlConnection(conn))
+            {
+                players=connection.Query<Player>("SELECT Id,Name,Score,IsStar FROM Players").ToList();
+            }
+            return players;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            var players=GetAll();
+            myDataGrid.ItemsSource = players;
+        }
+    }
+}
---
+++ WpfApp1/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿CREATE DATABASE GameDb
+GO
+USE GameDb
+CREATE TABLE Players(
+Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+Name NVARCHAR(30) NOT NULL,
+Score FLOAT NOT NULL,
+IsStar BIT NOT NULL DEFAULT(0)
+)
+GO
+INSERT INTO Players([Name],[Score],[IsStar])
+VALUES('Lebron',99,1),
+('Stephan Curry',95,1),
+('Maykl Jordan',95,0)
+
+SELECT * FROM Players
